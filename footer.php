b5853c69534976235f147cb8a54b30616aa921ee
khalinoid <khalinoid@gmail.com>
2024-06-04 17:07:35 +0300

    footer frontend

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
+<?php wp_footer()?>
 <footer id="main-footer">
+  <hr>
         <div class="footer-content">
             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
         </div>
-        <div class="container">
+        <div class="theme-container">
             <div class="footer-nav">
                 <ul>
                     <li><a href="#">@ All Copy Right are reserved 2024 AYLDC</a></li>
@@ -14,6 +16,5 @@
             </div>
         </div>
     </footer>
-    <?php wp_footer()?>
 </body>
 </html>
