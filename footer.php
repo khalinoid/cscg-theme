05d331a7465eb650433b3e91017e23df1f286c98
khalinoid <khalinoid@gmail.com>
2024-07-12 11:10:23 +0300

    footer texts

--- footer.php
+++ footer.php
@@ -2,18 +2,18 @@
 <footer id="main-footer">
   <hr>
         <div class="footer-content">
-            <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+            <!-- <p>&copy; < ?php echo date('Y'); ?> < ?php bloginfo('name'); ?></p> -->
         </div>
         <div class="theme-container">
             <div class="footer-nav">
                 <ul>
                     <li><a href="#">@ All Copy Right are reserved 2024 AYLDC</a></li>
-                    <li><a href="#">Refund Policy</a></li>
-                    <li><a href="#">Community Guidelines</a></li>
-                    <li><a href="#">Terms & Conditions</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
+                    <!-- <li><a href="/policy">Refund Policy</a></li> -->
+                    <li><a href="/community">Community Guidelines</a></li>
+                    <li><a href="/terms">Terms & Conditions</a></li>
+                    <li><a href="/privacy">Privacy Policy</a></li>
                 </ul>
-            </div>
+            </div> 
         </div>
     </footer>
 </body>
